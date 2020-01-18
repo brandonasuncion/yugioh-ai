7c539f237aa9858851c14eb1b8bc3b15d6bd651e
melvinzhang <melvinzhang@gmail.com>
2020-01-18 12:39:41 +0800

    add cli version that runs locally and gets intput from stdin

---
+++ cli.py
@@ -0,0 +1,121 @@
+import sqlite3
+import re
+re._pattern_type = re.Pattern
+
+from ygo import duel as dm
+from ygo import globals as glb
+from ygo import server
+from ygo.language_handler import LanguageHandler
+from ygo.duel_reader import DuelReader
+
+class Response:
+    def __init__(self, text):
+        self.text = text
+
+class FakeRoom:
+    def announce_draw(self):
+        pass
+
+    def announce_victory(self, player):
+        pass
+
+    def restore(self, player):
+        pass
+
+    def process(self):
+        pass
+
+class FakePlayer:
+    def __init__(self, i, deck):
+        self.deck = {"cards": deck}
+        self.duel_player = i
+        self.cdb = glb.server.db
+        self.language = "english"
+        self.watching = False
+        self.seen_waiting = False
+        self.soundpack = False
+
+    _ = lambda self, t: t
+
+    def notify(self, text, *args, **kwargs):
+        if text == DuelReader:
+            s = input()
+            caller = Response(s)
+            args[0](caller)
+        else:
+            print(self.duel_player, text)
+
+    @property
+    def strings(self):
+        return glb.language_handler.get_strings(self.language)
+
+class DumbAI(FakePlayer):
+    def notify(self, text, *args, **kwargs):
+        if text == DuelReader:
+            s = input()
+            caller = Response(s)
+            args[0](caller)
+        else:
+            print(self.duel_player, text)
+
+# from ygo/utils.py
+def process_duel(d):
+    while d.started:
+        res = d.process()
+        if res & 0x20000:
+            break
+
+def main():
+    deck = [
+        43096270,
+        43096270,
+        43096270,
+        97127906,
+        97127906,
+        97127906,
+        64428736,
+        64428736,
+        64428736,
+        #93221206,
+        #93221206,
+        #93221206,
+        #36821538,
+        #36821538,
+        #36821538,
+        #85639257,
+        #85639257,
+        #85639257,
+        #49881766,
+        #49881766,
+        #49881766,
+    ]
+
+    glb.language_handler = LanguageHandler()
+    glb.language_handler.add("english", "en")
+    glb.language_handler.set_primary_language("english")
+    glb.server = server.Server()
+    glb.server.db = sqlite3.connect('locale/en/cards.cdb')
+    glb.server.db.row_factory = sqlite3.Row
+
+    duel = dm.Duel()
+    duel.room = FakeRoom()
+    config = {"players": ["Human", "Machine"], "decks": [deck, deck]}
+    players = [FakePlayer(0, config["decks"][0]), DumbAI(1, config["decks"][1])]
+    for i, name in enumerate(config["players"]):
+        players[i].nickname = name
+        duel.load_deck(players[i])
+    duel.players = players
+    duel.set_player_info(0, 8000)
+    duel.set_player_info(1, 8000)
+    # rules = 0, Default
+    # rules = 1, Traditional
+    # rules = 4, Link
+    rules = 1
+    options = 0
+    duel.start(((rules & 0xFF) << 16) + (options & 0xFFFF))
+    process_duel(duel)
+    print("END")
+
+
+if __name__ == "__main__":
+    main()
