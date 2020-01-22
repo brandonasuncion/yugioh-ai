63a7628d3b4fcee6af9a5ef4bff60cc48193cbf0
melvinzhang <melvinzhang@gmail.com>
2020-01-22 11:37:53 +0800

    format with black

--- ygo/message_handlers/select_option.py
+++ ygo/message_handlers/select_option.py
@@ -6,54 +6,68 @@ from ygo.card import Card
 from ygo.parsers.duel_parser import DuelParser
 from ygo.utils import process_duel
 
+
 def msg_select_option(self, data):
-	data = io.BytesIO(data[1:])
-	player = self.read_u8(data)
-	size = self.read_u8(data)
-	options = []
-	for i in range(size):
-		options.append(self.read_u32(data))
-	self.cm.call_callbacks("select_option", player, options)
-	return data.read()
+    data = io.BytesIO(data[1:])
+    player = self.read_u8(data)
+    size = self.read_u8(data)
+    options = []
+    for i in range(size):
+        options.append(self.read_u32(data))
+    self.cm.call_callbacks("select_option", player, options)
+    return data.read()
+
 
 def select_option(self, player, options):
-	pl = self.players[player]
-	def select(caller, idx):
+    pl = self.players[player]
 
-		opt = options[idx]
+    def select(caller, idx):
 
-		for p in self.players+self.watchers:
+        opt = options[idx]
 
-			if opt > 10000:
-				string = card.get_strings(p)[opt & 0xf]
-			else:
-				string = p._("Unknown option %d" % opt)
-				string = p.strings['system'].get(opt, string)
+        for p in self.players + self.watchers:
 
-			if p is pl:
-				p.notify(p._("You selected option {0}: {1}").format(idx + 1, string))
-			else:
-				p.notify(p._("{0} selected option {1}: {2}").format(pl.nickname, idx + 1, string))
+            if opt > 10000:
+                string = card.get_strings(p)[opt & 0xF]
+            else:
+                string = p._("Unknown option %d" % opt)
+                string = p.strings["system"].get(opt, string)
 
-		self.set_responsei(idx)
-		reactor.callLater(0, process_duel, self)
+            if p is pl:
+                p.notify(p._("You selected option {0}: {1}").format(idx + 1, string))
+            else:
+                p.notify(
+                    p._("{0} selected option {1}: {2}").format(
+                        pl.nickname, idx + 1, string
+                    )
+                )
 
-	card = None
-	opts = []
-	for opt in options:
-		if opt > 10000:
-			code = opt >> 4
-			card = Card(code)
-			string = card.get_strings(pl)[opt & 0xf]
-		else:
-			string = pl._("Unknown option %d" % opt)
-			string = pl.strings['system'].get(opt, string)
-		opts.append(string)
-	m = Menu(pl._("Select option:"), no_abort=pl._("Invalid option."), prompt=pl._("Select option:"), persistent=True, restore_parser=DuelParser)
-	for idx, opt in enumerate(opts):
-		m.item(opt)(lambda caller, idx=idx: select(caller, idx))
-	pl.notify(m)
+        self.set_responsei(idx)
+        reactor.callLater(0, process_duel, self)
+
+    card = None
+    opts = []
+    for opt in options:
+        if opt > 10000:
+            code = opt >> 4
+            card = Card(code)
+            string = card.get_strings(pl)[opt & 0xF]
+        else:
+            string = pl._("Unknown option %d" % opt)
+            string = pl.strings["system"].get(opt, string)
+        opts.append(string)
+    m = Menu(
+        pl._("Select option:"),
+        no_abort=pl._("Invalid option."),
+        prompt=pl._("Select option:"),
+        persistent=True,
+        restore_parser=DuelParser,
+    )
+    for idx, opt in enumerate(opts):
+        m.item(opt)(lambda caller, idx=idx: select(caller, idx))
+    pl.notify(m)
+
 
 MESSAGES = {14: msg_select_option}
 
-CALLBACKS = {'select_option': select_option}
+CALLBACKS = {"select_option": select_option}
